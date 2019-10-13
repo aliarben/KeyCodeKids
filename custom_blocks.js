c3c61b4eb7c126a04f8cdf7b5db6c013fe97e8d1
aliarben <arben.ali@makpetrol.com.mk>
2019-10-13 21:28:22 +0200

    Add files via upload

--- custom_blocks.js
+++ custom_blocks.js
@@ -1,72 +1,253 @@
-Blockly.Blocks['kids_run_left'] = {
+//run_forward
+Blockly.Blocks['run_forward'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField(new Blockly.FieldImage("http://files.softicons.com/download/toolbar-icons/marmalade-icons-by-icojam/png/48x48/1_012.png", 60, 60, "*"));
+        .appendField(new Blockly.FieldImage("https://github.com/aliarben/KeyCodeKids/raw/master/img/forward.png", 50, 50, "*"));
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
-    this.setColour(60);
- this.setTooltip("Robot move left");
- this.setHelpUrl("");
-  }
-};
-
-Blockly.JavaScript['kids_run_left'] = function(block) {
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
-};
-
-Blockly.Blocks['kids_run_right'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField(new Blockly.FieldImage("http://files.softicons.com/download/toolbar-icons/marmalade-icons-by-icojam/png/48x48/1_018.png", 60, 60, "*"));
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(60);
- this.setTooltip("Robot move right");
- this.setHelpUrl("");
-  }
-};
-
-Blockly.JavaScript['kids_run_right'] = function(block) {
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
-};
-
-Blockly.Blocks['kids_run_backward'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField(new Blockly.FieldImage("http://files.softicons.com/download/toolbar-icons/marmalade-icons-by-icojam/png/48x48/1_017.png", 60, 60, "*"));
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(60);
- this.setTooltip("Robot move backward");
- this.setHelpUrl("");
-  }
-};
-
-Blockly.JavaScript['kids_run_backward'] = function(block) {
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
-};
-
-Blockly.Blocks['kids_run_forward'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField(new Blockly.FieldImage("http://files.softicons.com/download/toolbar-icons/marmalade-icons-by-icojam/png/48x48/1_028.png", 60, 60, "*"));
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(60);
+    this.setColour(230);
  this.setTooltip("Robot move forward");
  this.setHelpUrl("");
   }
 };
 
-Blockly.JavaScript['kids_run_forward'] = function(block) {
+Blockly.JavaScript['run_forward'] = function(block) {
   // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
+  var code = '#101{};\n';
+  return code;
+};
+
+//run_backward
+Blockly.Blocks['run_backward'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldImage("https://github.com/aliarben/KeyCodeKids/raw/master/img/backward.png", 50, 50, "*"));
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+ this.setTooltip("Move robot backward");
+ this.setHelpUrl("");
+  }
+}
+Blockly.JavaScript['run_backward'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '#102{};\n';
+  return code;
+};
+
+//turn_left
+Blockly.Blocks['turn_left'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldImage("https://github.com/aliarben/KeyCodeKids/raw/master/img/left.png", 50, 50, "*"));
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+ this.setTooltip("Turn robot left");
+ this.setHelpUrl("");
+  }
+}
+Blockly.JavaScript['turn_left'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '#103{};\n';
+  return code;
+};
+
+//turn_right
+Blockly.Blocks['turn_right'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldImage("https://github.com/aliarben/KeyCodeKids/raw/master/img/right.png", 50, 50, "*"));
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+ this.setTooltip("Turn robot left");
+ this.setHelpUrl("");
+  }
+}
+Blockly.JavaScript['turn_right'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '#104{};\n';
+  return code;
+};
+
+//stop
+Blockly.Blocks['stop'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldImage("https://github.com/aliarben/KeyCodeKids/raw/master/img/stop-cartoon.png", 50, 50, "*"));
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+ this.setTooltip("Turn robot left");
+ this.setHelpUrl("");
+  }
+}
+Blockly.JavaScript['stop'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '#106{};\n';
+  return code;
+};
+
+//led_red_on
+Blockly.Blocks['led_red_on'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldImage("https://github.com/aliarben/KeyCodeKids/raw/master/img/red_on.png", 50, 50, "*"));
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(260);
+ this.setTooltip("Red lights on");
+ this.setHelpUrl("");
+  }
+}
+Blockly.JavaScript['led_red_on'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '#107{1};\n';
+  return code;
+};
+
+//led_red_off
+Blockly.Blocks['led_red_off'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldImage("https://github.com/aliarben/KeyCodeKids/raw/master/img/red_off.png", 50, 50, "*"));
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(260);
+ this.setTooltip("Red lights off");
+ this.setHelpUrl("");
+  }
+}
+Blockly.JavaScript['led_red_off'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '#107{0};\n';
+  return code;
+};
+
+//led_green_on
+Blockly.Blocks['led_green_on'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldImage("https://github.com/aliarben/KeyCodeKids/raw/master/img/green_on.png", 50, 50, "*"));
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(260);
+ this.setTooltip("Green lights on");
+ this.setHelpUrl("");
+  }
+}
+Blockly.JavaScript['led_green_on'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '#109{1};\n';
+  return code;
+};
+
+//led_green_off
+Blockly.Blocks['led_green_off'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldImage("https://github.com/aliarben/KeyCodeKids/raw/master/img/green_off.png", 50, 50, "*"));
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(260);
+ this.setTooltip("Green lights off");
+ this.setHelpUrl("");
+  }
+}
+Blockly.JavaScript['led_green_off'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '#109{0};\n';
+  return code;
+};
+
+//led_blue_on
+Blockly.Blocks['led_blue_on'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldImage("https://github.com/aliarben/KeyCodeKids/raw/master/img/blue_on.png", 50, 50, "*"));
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(260);
+ this.setTooltip("Blue lights on");
+ this.setHelpUrl("");
+  }
+}
+Blockly.JavaScript['led_blue_on'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '#108{1};\n';
+  return code;
+};
+
+//led_blue_off
+Blockly.Blocks['led_blue_off'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldImage("https://github.com/aliarben/KeyCodeKids/raw/master/img/blue_off.png", 50, 50, "*"));
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(260);
+ this.setTooltip("Blue lights off");
+ this.setHelpUrl("");
+  }
+}
+Blockly.JavaScript['led_blue_off'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '#108{0};\n';
+  return code;
+};
+
+//light_show_on
+Blockly.Blocks['light_show_on'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldImage("https://github.com/aliarben/KeyCodeKids/raw/master/img/light_show_on.png", 50, 50, "*"));
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(260);
+ this.setTooltip("Light show on");
+ this.setHelpUrl("");
+  }
+}
+Blockly.JavaScript['light_show_on'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '#110{1};\n';
+  return code;
+};
+
+//light_show_off
+Blockly.Blocks['light_show_off'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldImage("https://github.com/aliarben/KeyCodeKids/raw/master/img/light_show_off.png", 50, 50, "*"));
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(260);
+ this.setTooltip("Light show off");
+ this.setHelpUrl("");
+  }
+}
+Blockly.JavaScript['light_show_off'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '#110{0};\n';
+  return code;
+};
+
+//dance_show_1
+Blockly.Blocks['dance_show_1'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldImage("https://github.com/aliarben/KeyCodeKids/raw/master/img/dance1.png", 50, 50, "*"));
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+ this.setTooltip("Dance show");
+ this.setHelpUrl("");
+  }
+}
+Blockly.JavaScript['dance_show_1'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '#111{};\n';
   return code;
 };
 
